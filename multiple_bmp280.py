4572c5cebd4f14683ae1de7f6c94c1f01f9bf9d7
zach.andrews <zach.andrews@madwiremedia.com>
2019-12-04 09:01:23 -0700

    refactored some code, added secrets, and texts

--- multiple_bmp280.py
+++ multiple_bmp280.py
@@ -2,30 +2,45 @@ import smbus2
 import bme280
 import time
 import alertlib
+from twilio.rest import Client
+class bmp:
+    def __init__(self,write_enabled):
+        f = open("secrets", "r")
+        creds = json.load(f)
 
-def calibrate(numb):
-    bus = smbus2.SMBus(numb)
-    return bme280.load_calibration_params(bus, 0x76)
+        self.accountSID = creds['accountSID']
+        self.authToken = creds['authToken']
+        self.toNumber = creds['toNumber']
+        self.fromNumber = creds['fromNumber']
+        self.toEmail = creds['toEmail']
 
-def get_data(numb,calibration):
-    bus = smbus2.SMBus(numb)
-    return bme280.sample(bus, 0x76, calibration)
+    def calibrate(numb):
+        bus = smbus2.SMBus(numb)
+        return bme280.load_calibration_params(bus, 0x76)
 
-def print_data(data):
-    print("Reading sensor on i2c bus 3 at time %f" % data.timestamp)
-    print "Temperature : %.2f" %data.temperature
-    print "Pressure : %.2f hPa " %data.pressure+"\n"
+    def get_data(numb,calibration):
+        bus = smbus2.SMBus(numb)
+        return bme280.sample(bus, 0x76, calibration)
 
-calibration3 = calibrate(3)
-calibration4 = calibrate(4)
+    def print_data(data):
+        print("Reading sensor on i2c bus 3 at time %f" % data.timestamp)
+        print "Temperature : %.2f" %data.temperature
+        print "Pressure : %.2f hPa " %data.pressure+"\n"
 
-while True:
-    inside_sensor = get_data(3,calibration3)
-    outside_sensor = get_data(4,calibration4)
-    if inside_sensor.pressure/outside_sensor.pressure > 1.1:
-        print "warning"
-        alert = alertlib.Alert("The pressure sensors are out of sync!")
-        alert.send_to_email("zandrews22@gmail.com --summary Pressure sensors")
-    time.sleep(60)
-    
+    calibration3 = calibrate(3)
+    calibration4 = calibrate(4)
 
+    client = Client(self.accountSID,self.authToken)
+
+    while True:
+        inside_sensor = get_data(3,calibration3)
+        outside_sensor = get_data(4,calibration4)
+        if inside_sensor.pressure/outside_sensor.pressure > 1.1:
+            print "warning"
+            alert = alertlib.Alert("The pressure sensors are out of sync!")
+            alert.send_to_email(self.toEmail + " --summary Pressure sensors")
+
+            client.messages.create(to=self.toNumber,
+                                from_=self.fromNumber,
+                                body="The Pressure sensors are out of sync!")
+        time.sleep(60)
